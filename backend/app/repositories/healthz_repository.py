ef7ae34b79ad15eb3e3248de1ef1cb949fc6362b
jinsoo.lim <ljslim27@gmail.com>
2026-04-09 16:05:09 +0900

    fix: healthz 요청시  DB 컬럼 조회하여 헬스 체크 기능 완성

--- backend/app/repositories/healthz_repository.py
+++ backend/app/repositories/healthz_repository.py
@@ -4,8 +4,7 @@ class HealthzRepository:
     def __init__(self, supabase: Client):
         self.supabase = supabase
 
-    def health_check(self, schema:str, table:str):
+    def health_check(self, schema_name:str, table_name:str):
         """요청 받은 '스미카'와 '테이블'의 데이터를 1건 조회 한다 -> health check"""
-        resp = self.supabase.schema(schema).table(table).select("*").limit(1).execute()
+        resp = self.supabase.schema(schema_name).table(table_name).select("*").limit(1).execute()
         return resp.data    
-
--- backend/app/services/healthz_service.py
+++ backend/app/services/healthz_service.py
@@ -13,12 +13,12 @@ class HealthzService:
     ):
         # schema = payload.schema or "vehicle_manual_rag"
         # table = payload.table  or "models"
-        schema = payload.schema
-        table = payload.table
+        schema_name = payload.schema_name
+        table_name = payload.table_name
 
         # DB작업은 repositories를 통해서 진행합니다.
         repo = HealthzRepository(supabase)
-        data = repo.health_check(schema, table)
+        data = repo.health_check(schema_name, table_name)
         if not data: 
             # 데이터가 없을 경우 HTTPException
             raise HTTPException(status_code=404, detail="Data not found")
